fd8ed420cb1ffa0a2034fcf2eef7522d7776a08b
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-11-24 07:27:15 +0100

    aspitalia

---
+++ Aspitalia/DotNetConf2020.GraphNet5/Graph/GraphHelper.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetConf2020.GraphNet5
+{
+    public class GraphHelper
+    {
+        private static GraphServiceClient graphClient;
+        public static void Initialize(IAuthenticationProvider authProvider)
+        {
+            graphClient = new GraphServiceClient(authProvider);
+        }
+        public static async Task<User> GetMeAsync()
+        {
+            try
+            {
+                return await graphClient.Me.Request().GetAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+        public static async Task SendMailAsync()
+        {
+            try
+            {
+                var message = new Message
+                {
+                    Subject = "Testing from DotNetConf 2020",
+                    Body = new ItemBody
+                    {
+                        ContentType = BodyType.Text,
+                        Content = "The Graph SDK is great!"
+                    },
+                    ToRecipients = new List<Recipient>()
+                    {
+                        new Recipient
+                        {
+                            EmailAddress = new EmailAddress
+                            {
+                                Address = "eba@expertsinside.com"
+                            }
+                        }
+                    }
+                };
+
+                await graphClient.Me
+                    .SendMail(message, true)
+                    .Request()
+                    .PostAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+            }
+        }
+
+        public static async Task<IUserJoinedTeamsCollectionPage> GetTeamsAsync()
+        {
+            try
+            {
+                var teams = await graphClient.Me.JoinedTeams
+                    .Request()
+                    .GetAsync();
+
+                return teams;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ Aspitalia/DotNetConf2020.GraphNet5/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace DotNetConf2020.GraphNet5
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var appConfig = LoadAppSettings();
+            
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var scopes = scopesString.Split(';');
+            var authProvider = new DeviceCodeAuthProvider(appId, scopes);
+            
+            var accessToken = await authProvider.GetAccessToken();
+            Console.WriteLine($"Access token: {accessToken}\n");
+            GraphHelper.Initialize(authProvider);
+
+            var user = await GraphHelper.GetMeAsync();
+            Console.WriteLine($"Welcome {user.DisplayName}!\n");
+
+            var teams = await GraphHelper.GetTeamsAsync();
+
+            foreach (var team in teams)
+            {
+                Console.WriteLine($"Team: {team.DisplayName}!\n");
+            }
+
+            await GraphHelper.SendMailAsync();
+
+            Console.WriteLine("Email sent!");
+
+            Console.ReadLine();
+        }
+
+        static IConfigurationRoot LoadAppSettings()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                string.IsNullOrEmpty(appConfig["scopes"]))
+            {
+                return null;
+            }
+            return appConfig;
+        }
+    }
+}
